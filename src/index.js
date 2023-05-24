b4195400d785650488b44dc3f11a5eafd43009a9
berylcl <gakiiberyl@gmail.com>
2023-05-24 16:00:49 +0100

    Push changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { getMeals, fetchMealDetails } from './modules/api.js';
+import { getMeals, fetchMealDetails, getComments} from './modules/api-comments.js';
 
 const displayMeals = async () => {
   const meals = await getMeals();
@@ -27,38 +27,47 @@ const createMeal = (meal) => `
       </div>
     </div>
   `;
+const truncateDescription = (description, wordCount) => {
+  const words = description.split(' ');
+  if (words.length <= wordCount) {
+    return description;
+  }
+  const truncatedWords = words.slice(0, wordCount);
+  return truncatedWords.join(' ') + '...';
+};
+
 const showCommentsPopup = async (mealId) => {
   // Create the popup element
   const popupContent = document.createElement('div');
   popupContent.classList.add('launch-card');
   popupContent.innerHTML = `
-  <button class="close-modal">&times;</button>
-  <div class="launch-img">
-    <img class="image-popup" src="" alt="meal image" />
-  </div>
-  <div id="comment">
-    <h2></h2>
-    <h2>Comments (<span class="comments-count"></span>)</h2>
-    <ul class="commentList"></ul>
-    <div class="entry">
-      <h2>Add a Comment</h2>
-      <form>
-        <div class="form-group">
-          <label for="txt-visitor">Name:</label>
-          <input id="txt-visitor" type="text" placeholder="Your name" />
-        </div>
-        <div class="form-group">
-          <label for="txt-comment">Comment:</label>
-          <input id="txt-comment" type="text" placeholder="Your comment" />
-        </div>
-        <input id="launch-id" type="hidden" />
-        <div class="form-group">
-          <input class="comments" id="btn-save-comment" type="button" value="Comment" />
-        </div>
-      </form>
+    <button class="close-modal">&times;</button>
+    <div class="launch-img">
+      <img class="image-popup" src="" alt="meal image" />
     </div>
-  </div>
-`;
+    <div id="comment">
+      <h2></h2>
+      <h2>Comments (<span class="comments-count"></span>)</h2>
+      <ul class="commentList"></ul>
+      <div class="entry">
+        <h2>Add a Comment</h2>
+        <form>
+          <div class="form-group">
+            <label for="txt-visitor">Name:</label>
+            <input id="txt-visitor" type="text" placeholder="Your name" />
+          </div>
+          <div class="form-group">
+            <label for="txt-comment">Comment:</label>
+            <input id="txt-comment" type="text" placeholder="Your comment" />
+          </div>
+          <input id="launch-id" type="hidden" />
+          <div class="form-group">
+            <input class="comments" id="btn-save-comment" type="button" value="Comment" />
+          </div>
+        </form>
+      </div>
+    </div>
+  `;
 
   popupContent.style.display = 'none';
 
@@ -73,41 +82,49 @@ const showCommentsPopup = async (mealId) => {
     }
   });
 
-  const showCommentsPopup = async (mealId) => {
-    try {
-      const mealDetails = await fetchMealDetails(mealId);
-      const mealImage = popupContent.querySelector('.image-popup');
-      const mealName = popupContent.querySelector('h2');
+  try {
+    const mealDetails = await fetchMealDetails(mealId);
+    const mealImage = popupContent.querySelector('.image-popup');
+    const mealName = popupContent.querySelector('h2');
+    const mealDescription = popupContent.querySelector('.entry > h2');
+    const truncatedDescription = truncateDescription(mealDetails.strInstructions, 60); // Adjust the word count as desired
 
-      mealImage.src = mealDetails.strMealThumb;
-      mealName.textContent = mealDetails.strMeal;
+    mealImage.src = mealDetails.strMealThumb;
+    mealName.textContent = mealDetails.strMeal;
+    mealDescription.textContent = truncatedDescription;
 
-      // Remove any existing popup before showing a new one
-      const existingPopup = document.querySelector('.launch-card');
-      if (existingPopup) {
-        existingPopup.remove();
-      }
-
-      // Append the popup to the document body
-      document.body.appendChild(popupContent);
-
-      // Show the popup
-      popupContent.style.display = 'block';
-
-      console.log(mealDetails);
-    } catch (error) {
-      console.error('Error fetching meal details:', error);
+    // Remove any existing popup before showing a new one
+    const existingPopup = document.querySelector('.launch-card');
+    if (existingPopup) {
+      existingPopup.remove();
     }
-  };
 
-  document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('comments')) {
-      const { mealId } = event.target.closest('.meal').dataset;
-      showCommentsPopup(mealId);
-    }
-  });
+    // Append the popup to the document body
+    document.body.appendChild(popupContent);
 
+    // Show the popup
+    popupContent.style.display = 'block';
+
+    console.log(mealDetails);
+  } catch (error) {
+    console.error('Error fetching meal details:', error);
+  }
 };
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('comments')) {
+    const { mealId } = event.target.closest('.meal').dataset;
+    showCommentsPopup(mealId);
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('comments')) {
+    const { mealId } = event.target.closest('.meal').dataset;
+    showCommentsPopup(mealId);
+  }
+});
+
 
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('comments')) {
--- src/modules/api-comments.js
+++ src/modules/api-comments.js
@@ -1,5 +1,7 @@
 const baseApi = 'https://www.themealdb.com/api/json/v1/1';
+const InvolvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
 
+const apiId = 'peHlM9hq9qKvsrh6N3Wm';
 export const getMeals = async () => {
   const response = await fetch(`${baseApi}/filter.php?a=Jamaican`);
   const { meals } = await response.json();
@@ -13,4 +15,13 @@ export const fetchMealDetails = async (mealId) => {
     return meals[0];
   }
   throw new Error('Meal details not found.');
+};
+export const getComments = async (mealId) => {
+  try {
+    const response = await fetch(`${InvolvementApi}/apps/${apiId}/comments?item_id=${mealId}`);
+    const { comments } = await response.json();
+    return comments;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
